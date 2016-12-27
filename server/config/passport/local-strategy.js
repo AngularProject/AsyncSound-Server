8ba04464d1e48d6f95d0ff0383fbe87ff9eaa0a7
Galvaneyes <m.v.manchev@gmail.com>
2016-12-27 11:17:37 +0200

    Added crypto for password

--- server/config/passport/local-strategy.js
+++ server/config/passport/local-strategy.js
@@ -4,16 +4,16 @@
 const LocalStrategy = require("passport-local");
 const encrypt = require("../../utils/encrypt");
 
-// function authenticate(user, pswd) {
-//     return encrypt.hashPassword(user.salt, pswd) === user.passHash;
-// }
+function authenticate(user, pswd) {
+    return encrypt.hashPassword(user.salt, pswd) === user.passHash;
+}
 
 module.exports = function(passport, data) {
     const authStrategy = new LocalStrategy((username, password, done) => {
         data.getUserByUsername(username)
             .then(user => {
-                //if (user && authenticate(user, password)) {
-                if(user && (user.password === password)) {
+                if (user && authenticate(user, password)) {
+                // if(user && (user.password === password)) {
                     return done(null, user);
                 }
 
