cdba440e08b55ec53e8b1cbaff865b19aed06eff
Galvaneyes <m.v.manchev@gmail.com>
2016-12-27 11:23:52 +0200

    Added salt and password hash

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: { type: String, validate: /[a-zA-Z0-9]+/, required: true, unique: true },
-    password: { type: String, required: true },
+    salt: { type: String, required: true },
+    passHash: { type: String, required: true },
     email: { type: String },
     firstname: { type: String, validate: /[a-zA-Z]+/, required: true },
     lastname: { type: String, validate: /[a-zA-Z]+/, required: true },
