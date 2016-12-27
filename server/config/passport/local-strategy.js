31b006e7f4365ea17c3fcb843ff2ff8d242d422b
Galvaneyes <m.v.manchev@gmail.com>
2016-12-27 03:09:43 +0200

    Set local strategy configuration

---
+++ server/config/passport/local-strategy.js
@@ -0,0 +1,26 @@
+/* globals module require */
+"use strict";
+
+const LocalStrategy = require("passport-local");
+const encrypt = require("../../utils/encrypt");
+
+// function authenticate(user, pswd) {
+//     return encrypt.hashPassword(user.salt, pswd) === user.passHash;
+// }
+
+module.exports = function(passport, data) {
+    const authStrategy = new LocalStrategy((username, password, done) => {
+        data.getUserByUsername(username)
+            .then(user => {
+                //if (user && authenticate(user, password)) {
+                if(user && (user.password === password)) {
+                    return done(null, user);
+                }
+
+                return done(null, false);
+            })
+            .catch(err => done(err, false));
+    });
+
+    passport.use(authStrategy);
+};
