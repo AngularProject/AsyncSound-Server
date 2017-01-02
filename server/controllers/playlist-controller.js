a23ef40c3fb5bd66093bb5d4843018d8ae1457c6
TeodSt <teod_stoyanova@abv.bg>
2017-01-02 23:04:55 +0200

    User has playlists! He is cool now

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -5,9 +5,16 @@ module.exports = function({ data }) {
     return {
         addNewPlaylist(req, res) {
             let playlistInfo = req.body;
-            console.log(playlistInfo.creator.username);
+            // let id;
+            // console.log(playlistInfo.creator.username);
             return data.createPlaylist(playlistInfo)
                 .then(playlist => {
+                    // id = playlist._id;
+                    let query = { username: playlistInfo.creator };
+                    let updateObject = { $push: { playlists: playlist._id } };
+                    return data.findUserAndUpdate(updateObject, query);
+                })
+                .then((user) => {
                     res.status(200).json({ succes: true, message: "Add some music now!" });
                 });
         },
@@ -35,12 +42,16 @@ module.exports = function({ data }) {
                 user,
                 id
             };
+            let query = { username: user };
+            let updateObject = { $push: { playlists: id } };
 
-            return data.addUserToPlaylist(info)
-                .then((playlist) => {
-                    console.log(playlist);
-                    res.status(200).json({ succes: true, message: "Playlist pinned" });
-                })
+            return data.findUserAndUpdate(updateObject, query)
+
+            // return data.addUserToPlaylist(info)
+            .then((user) => {
+                console.log(playlist);
+                res.status(200).json({ succes: true, message: user.name + " Playlist pinned" });
+            })
         }
     };
 };
